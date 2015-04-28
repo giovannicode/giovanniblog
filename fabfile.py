fbdf53727aba94c3bdce5856d6a99b8acb7059ce
drone-xb81 <giovannicode7@gmail.com>
2015-04-28 17:07:12 -0500

    Create fabfile.py for deployment purposes.

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import local, env, cd, run
+from fabric.context_managers import lcd
+
+env.hosts =['giovannicode.com']
+env.user = "root"
+
+
+def provision_server():
+    local("ansible-playbook -u root provisioning/production.yml")
+
+def push_code():
+    local("git push production master")
+
+def restart_gunicorn():
+    local("restart.yml")
+
+
+def deploy():
+    provision_server()
+    push_code()
+    restart_gunicorn()
